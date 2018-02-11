4ed26dbf4e018a41af893748743a47158f31c91e
Mike Calhoun <mikedcalhoun@gmail.com>
2018-02-10 23:46:19 -0800

    Add Atwork to Album

--- app/workers/spotify_worker.rb
+++ app/workers/spotify_worker.rb
@@ -7,9 +7,16 @@ class SpotifyWorker
     album = Album.find(album_id)
     spotify_artist = spotify_artist(album.artist_name)
     spotify_album = spotify_album(spotify_artist, album.title)
+    spotify_artwork(spotify_album)
     tracks(spotify_album).each_with_index do |track, index|
       Track.create(album_id: album.id, title: track.name, number: (index + 1))
     end
+  end
+
+  def spotify_artwork(album)
+    return unless album.present?
+    album.image_url = album.instance_variable_get('@images').first['url']
+    album.save
   end
 
   def spotify_artist(artist_name)

f1ed3e3291e5adb625d51ca4422458de65f01be8
Mike Calhoun <mikedcalhoun@gmail.com>
2018-02-11 00:54:49 -0800

    Fix SpotifyWorker Bug

--- app/workers/spotify_worker.rb
+++ app/workers/spotify_worker.rb
@@ -1,21 +1,20 @@
 class SpotifyWorker
   include Sidekiq::Worker
   require 'rspotify'
-  sidekiq_options retry: false
 
   def perform(album_id)
     album = Album.find(album_id)
     spotify_artist = spotify_artist(album.artist_name)
     spotify_album = spotify_album(spotify_artist, album.title)
-    spotify_artwork(spotify_album)
+    spotify_artwork(album, spotify_album)
     tracks(spotify_album).each_with_index do |track, index|
       Track.create(album_id: album.id, title: track.name, number: (index + 1))
     end
   end
 
-  def spotify_artwork(album)
+  def spotify_artwork(album, sa)
     return unless album.present?
-    album.image_url = album.instance_variable_get('@images').first['url']
+    album.image_url = sa.instance_variable_get('@images').first['url']
     album.save
   end
 
